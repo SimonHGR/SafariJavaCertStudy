06282f09de93865d25701a72f0e1a91d8f732581
simon <simon@dancingcloudservices.com>
2024-10-03 06:17:40 -0600

    Questions

--- src/main/java/nestedstuff/Ex2.java
+++ src/main/java/nestedstuff/Ex2.java
@@ -1,14 +1,19 @@
 package nestedstuff;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
-import static java.lang.annotation.ElementType.*;
-
-@Target(TYPE_USE)
-@interface NotNull {}
+//@Target(value={ElementType.TYPE_USE})
+//@Target(value=ElementType.TYPE_USE)
+@Target(ElementType.TYPE_USE)
+@Retention(RetentionPolicy.RUNTIME)
+@interface NotNull {
+  String fruit();
+}
 
 class Outer2 {
   class Inner2 {
@@ -19,7 +24,7 @@ class Outer2 {
 //      myEnclosingOuter2 = Outer2.this;
     }
 
-    void doStuff(@NotNull Inner2 this) {
+    void doStuff(@NotNull(fruit="banana") /* ahah ! */ Inner2 this) {
 //      Outer2.this = null;
       System.out.println(Outer2.this);
     }
